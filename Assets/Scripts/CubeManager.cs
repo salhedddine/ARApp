02e2dc937660a447d4f43b06fe36df480b3787f3
salheddine <salah-eddine@windowslive.com>
2020-12-16 15:38:15 +0100

    Shooting

---
+++ Assets/Scripts/CubeManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class CubeManager : MonoBehaviour
+{
+    GameObject A1;
+    float T;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        T = Time.time;
+
+        print(gameObject.name);
+
+        A1 = GetComponent<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float T2;
+        T2 = Time.time - T;
+        if (T2 > 3)
+        {
+            Destroy(gameObject);
+            T = Time.time;
+
+        }
+    }
+}
---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using System.Runtime.InteropServices;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject cube;
+    public GameObject bullet;
+    Rigidbody C;
+    public Transform CameraPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*
+        cube.transform.position = CameraPos.position + Vector3.forward * 0.5f;
+        C = cube.GetComponent <Rigidbody>();
+        C.useGravity = false;
+        cube.SetActive(false);
+
+        //cube.GetComponent<Rigidbody>() = C;
+        */
+    }    
+    // Update is called once per frame
+    void Update()
+    {
+        //bullet = Instantiate(cube);
+    }
+
+    public void CreateCube()
+    {
+        bullet = Instantiate(cube);
+        bullet.transform.position = CameraPos.position + Vector3.forward * 0.5f;
+        C = bullet.GetComponent<Rigidbody>();
+        cube.SetActive(true);
+        C.useGravity = true;
+        C.AddForce(transform.forward * 500);
+    }
+
+}
